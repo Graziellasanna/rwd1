7b73ef44803a4776158517c4e82ec3d2289ff468
Graziellasanna <dancafg@gmail.com>
2017-04-04 19:16:52 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque diam nunc, semper quis metus eget, auctor posuere felis. Integer ante lorem, auctor ut dui ut, lobortis vestibulum urna. Quisque urna lorem, consectetur eu venenatis nec, laoreet in dolor. Mauris placerat aliquet augue, vel porttitor lorem interdum a. Sed molestie sed ipsum suscipit dapibus. Curabitur dui diam, malesuada quis ullamcorper rhoncus, semper dictum turpis. Sed vitae faucibus nulla, sit amet iaculis quam. Duis eu venenatis nibh. Suspendisse egestas eleifend turpis vel bibendum. Sed gravida dapibus placerat.";
+
+lorem = lorem + lorem + lorem;
+
+$(".lorem1").html(lorem) + '<img src="images/ghilarza.san giovanni.jpg">';
+$(".lorem2").html(lorem) + '<img src="images/ghilarza.san_michele.jpg">' ;
+$(".lorem3").html(lorem) +'<img src="images/ghilarza.san_serafino.jpg">' ;
+    
